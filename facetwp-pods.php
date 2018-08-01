6456a42cb1ef1df55a887954e2ff4bd89c38acae
mgibbs189 <mgibbs189@gmail.com>
2018-08-01 11:59:53 -0400

    Load all Pods fields (maybe?)

--- facetwp-pods.php
+++ facetwp-pods.php
@@ -62,10 +62,21 @@ class FacetWP_Pods_Addon
 
 
     /**
-     * @todo Get all Pods fields
+     * Get all Pods fields
      */
     function get_fields() {
+        $fields = array();
 
+        $pods_api = pods_api();
+        $pod_names = $pods_api->load_pods( array( 'names' => true ) );
+        foreach ( $pod_names as $pod_name ) {
+            $pod_fields = $pods_api->load_fields( array( 'pod' => $pod_name ) );
+            foreach ( $pod_fields as $field ) {
+                $fields[] = $field;
+            }
+        }
+
+        return $fields;
     }
 
 
