606100bdc15a29bade71a0c9b0259f7e2dd9a13f
Scott Kingsley Clark <lol@scottkclark.com>
2018-08-08 07:42:08 -0500

    Refactor add-on code, handle field value indexing, add integration with User Post Type add-on (#5)
    * Refactor addon code, hold onto current pod, simplify value/display value logic
    
    * Add integration with User Post Type add-on; confirm source pod matches current post id
    
    * Exclude Pods internal custom fields for it's post types
    
    * More custom field meta_key exclusions
    
    * Fixes from further testing

--- facetwp-pods.php
+++ facetwp-pods.php
@@ -10,232 +10,355 @@ GitHub URI: facetwp/facetwp-pods
 
 defined( 'ABSPATH' ) or exit;
 
-class FacetWP_Pods_Addon
-{
-    /**
-     * @var array Fields data.
-     */
-    public $fields = array();
+/**
+ * Class FacetWP_Pods_Addon
+ *
+ * @since 0.1
+ */
+class FacetWP_Pods_Addon {
 
+	/**
+	 * @var array Fields data.
+	 *
+	 * @since 0.1
+	 */
+	public $fields = array();
 
-    public function __construct() {
-        add_action( 'init', array( $this, 'init' ) );
-    }
+	/**
+	 * FacetWP_Pods_Addon constructor.
+	 *
+	 * @since 0.1
+	 */
+	public function __construct() {
+		add_action( 'init', array( $this, 'init' ) );
+	}
 
+	/**
+	 * Handle hooks.
+	 *
+	 * @since 0.1
+	 */
+	public function init() {
+		add_filter( 'facetwp_facet_sources', array( $this, 'facet_sources' ) );
+		add_filter( 'facetwp_indexer_query_args', array( $this, 'lookup_fields' ) );
+		add_filter( 'facetwp_indexer_row_data', array( $this, 'get_index_rows' ), 1, 2 );
+		add_filter( 'facetwp_excluded_custom_fields', array( $this, 'exclude_internal_fields' ) );
+	}
 
-    public function init() {
-        add_filter( 'facetwp_facet_sources', array( $this, 'facet_sources' ) );
-        add_filter( 'facetwp_indexer_query_args', array( $this, 'lookup_pods_fields' ) );
-        add_filter( 'facetwp_indexer_post_facet', array( $this, 'index_pods_values' ), 1, 2 );
-        add_filter( 'facetwp_acf_display_value', array( $this, 'index_source_other' ), 1, 2 );
-    }
+	/**
+	 * Add Pods fields to sources list.
+	 *
+	 * @param $sources Sources list.
+	 *
+	 * @return array Sources list with Pods fields added.
+	 *
+	 * @since 0.1
+	 */
+	public function facet_sources( $sources ) {
+		$this->setup_fields();
 
+		$sources['pods'] = array(
+			'label'   => 'Pods',
+			'choices' => array(),
+			'weight'  => 5
+		);
 
-    /**
-     * Add Pods fields to sources list.
-     *
-     * @param $sources Sources list.
-     *
-     * @return array Sources list with Pods fields added.
-     */
-    public function facet_sources( $sources ) {
-        if ( empty( $this->fields ) ) {
-            $this->fields = $this->get_fields();
-        }
+		foreach ( $this->fields as $choice_id => $field ) {
+			$choice_label = sprintf( '[%1$s] %2$s', $field['pod_label'], $field['label'] );
 
-        $sources['pods'] = array(
-            'label' => 'Pods',
-            'choices' => array(),
-            'weight' => 5
-        );
+			$sources['pods']['choices'][ $choice_id ] = $choice_label;
+		}
 
-        foreach ( $this->fields as $choice_id => $field ) {
-            $choice_label = sprintf( '[%1$s] %2$s', $field['pod_label'], $field['label'] );
+		return $sources;
+	}
 
-            $sources['pods']['choices'][ $choice_id ] = $choice_label;
-        }
+	/**
+	 * Hijack the "facetwp_indexer_query_args" hook to lookup the fields once.
+	 *
+	 * @param array $args Arguments.
+	 *
+	 * @return array Arguments.
+	 *
+	 * @since 0.1
+	 */
+	public function lookup_fields( $args ) {
+		$this->setup_fields();
 
-        return $sources;
-    }
+		return $args;
+	}
 
+	/**
+	 * Setup all post type Pods fields.
+	 *
+	 * @return array Pods fields.
+	 *
+	 * @since 0.1
+	 */
+	public function setup_fields() {
+		if ( ! empty( $this->fields ) ) {
+			return $this->fields;
+		}
 
-    /**
-     * Hijack the "facetwp_indexer_query_args" hook to lookup the fields once
-     */
-    public function lookup_pods_fields( $args ) {
-        $this->fields = $this->get_fields();
+		$fields = array();
 
-        return $args;
-    }
+		$pods_api = pods_api();
 
+		$params = array(
+			'type'       => 'post_type',
+			'table_info' => false,
+		);
 
-    /**
-     * Get all post type Pods fields.
-     *
-     * @return array Pods fields.
-     */
-    public function get_fields() {
-        $fields = array();
+		$post_type_pods = $pods_api->load_pods( $params );
 
-        $params = array(
-            'type'       => 'post_type',
-            'table_info' => false,
-        );
+		if ( function_exists( 'UPT' ) ) {
+			// Check for user pod.
+			$params = array(
+				'name'       => 'user',
+				'table_info' => false,
+			);
 
-        $post_type_pods = pods_api()->load_pods( $params );
+			$user_pod = $pods_api->load_pod( $params );
 
-        foreach ( $post_type_pods as $pod ) {
-            foreach ( $pod['fields'] as $field ) {
-                $field['pod_label'] = $pod['label'];
-            
-                $choice_id = sprintf( 'pods/%1$s/%2$s', $field['pod'], $field['name'] );
-                
-                $fields[ $choice_id ] = $field;
-            }
-        }
+			// If we have a user pod, add it to the list.
+			if ( $user_pod ) {
+				$post_type_pods[] = $user_pod;
+			}
+		}
 
-        return $fields;
-    }
+		foreach ( $post_type_pods as $pod ) {
+			foreach ( $pod['fields'] as $field ) {
+				$field['pod_label'] = $pod['label'];
 
+				// If this is the user pod, set the compatible post type.
+				if ( 'user' === $field['pod'] ){
+					$field['pod'] = 'upt_user';
+				}
 
-    /**
-     * @todo Index Pods data
-     */
-    public function index_pods_values( $return, $params ) {
-        $defaults = $params['defaults'];
-        $facet = $params['facet'];
+				$choice_id = sprintf( 'pods/%1$s/%2$s', $field['pod'], $field['name'] );
 
-        if ( isset( $facet['source'] ) && 0 === strpos( $facet['source'], 'pods/' ) ) {
-            $props = explode( '/', $facet['source'] );
-            $pod_name = $props[1];
-            $field_name = $props[2];
-            $object_id = $defaults['post_id'];
+				$fields[ $choice_id ] = $field;
+			}
+		}
 
-            // get field value
-            $value = pods( $pod_name, $object_id )->field( array(
-                'name' => $field_name
-            ) );
+		$this->fields = $fields;
 
-            // get field properties
-            $field = pods_api()->load_field( array(
-                'pod' => $pod_name,
-                'name' => $field_name
-            ) );
+		return $fields;
+	}
 
-            // index values
-            $this->index_field_value( $value, $field, $defaults );
+	/**
+	 * Handle indexing of pods/{pod}/{field} sources.
+	 *
+	 * @param array $rows   Rows to index.
+	 * @param array $params Index parameters.
+	 *
+	 * @param array Rows to index.
+	 *
+	 * @since 0.1
+	 */
+	public function get_index_rows( $rows, $params ) {
+		$defaults = $params['defaults'];
+		$facet    = $params['facet'];
 
-            return true;
-        }
+		if ( ! isset( $facet['source'] ) || 0 !== strpos( $facet['source'], 'pods/' ) ) {
+			return $rows;
+		}
 
-        return $return;
-    }
+		// pods/{pod_name}/{field_name}
+		$props = explode( '/', $facet['source'] );
 
+		$pod_name   = $props[1];
+		$field_name = $props[2];
 
-    /**
-     * @todo index values depending on the field type
-     */
-    public function index_field_value( $value, $field, $params ) {
-        $value = maybe_unserialize( $value );
+		$item_id = $defaults['post_id'];
 
-        // checkboxes
-        if ( 'checkbox' == $field['type'] || 'select' == $field['type'] || 'radio' == $field['type'] ) {
-            if ( false !== $value ) {
-                foreach ( (array) $value as $val ) {
-                    $display_value = isset( $field['choices'][ $val ] ) ?
-                        $field['choices'][ $val ] :
-                        $val;
+		$post = get_post( $item_id );
 
-                    $params['facet_value'] = $val;
-                    $params['facet_display_value'] = $display_value;
-                    FWP()->indexer->index_row( $params );
-                }
-            }
-        }
+		// Check if this matches the source post type.
+		if ( ! $post || $pod_name !== $post->post_type ) {
+			return $rows;
+		}
 
-        // relationship
-        elseif ( 'relationship' == $field['type'] || 'post_object' == $field['type'] ) {
-            if ( false !== $value ) {
-                foreach ( (array) $value as $val ) {
-                    $params['facet_value'] = $val;
-                    $params['facet_display_value'] = get_the_title( $val );
-                    FWP()->indexer->index_row( $params );
-                }
-            }
-        }
+		// Check if this is a compatible user pod.
+		if ( 'upt_user' === $pod_name ) {
+			// Integration is not available.
+			if ( ! function_exists( 'UPT' ) ) {
+				return $rows;
+			}
 
-        // user
-        elseif ( 'user' == $field['type'] ) {
-            if ( false !== $value )  {
-                foreach ( (array) $value as $val ) {
-                    $user = get_user_by( 'id', $val );
-                    if ( false !== $user ) {
-                        $params['facet_value'] = $val;
-                        $params['facet_display_value'] = $user->display_name;
-                        FWP()->indexer->index_row( $params );
-                    }
-                }
-            }
-        }
+			// Set the real pod name.
+			$pod_name = 'user';
 
-        // taxonomy
-        elseif ( 'taxonomy' == $field['type'] ) {
-            if ( ! empty( $value ) ) {
-                foreach ( (array) $value as $val ) {
-                    global $wpdb;
+			// Get the real user ID.
+			$item_id = UPT()->get_user_id( $item_id );
 
-                    $term_id = (int) $val;
-                    $term = $wpdb->get_row( "SELECT name, slug FROM {$wpdb->terms} WHERE term_id = '$term_id' LIMIT 1" );
-                    if ( null !== $term ) {
-                        $params['facet_value'] = $term->slug;
-                        $params['facet_display_value'] = $term->name;
-                        $params['term_id'] = $term_id;
-                        FWP()->indexer->index_row( $params );
-                    }
-                }
-            }
-        }
+			if ( ! $item_id ) {
+				return $rows;
+			}
+		}
 
-        // date_picker
-        elseif ( 'date_picker' == $field['type'] ) {
-            $formatted = $this->format_date( $value );
-            $params['facet_value'] = $formatted;
-            $params['facet_display_value'] = apply_filters( 'facetwp_acf_display_value', $formatted, $params );
-            FWP()->indexer->index_row( $params );
-        }
+		$pod = $this->setup_pod( $pod_name, $item_id );
 
-        // true_false
-        elseif ( 'true_false' == $field['type'] ) {
-            $display_value = ( 0 < (int) $value ) ? __( 'Yes', 'fwp' ) : __( 'No', 'fwp' );
-            $params['facet_value'] = $value;
-            $params['facet_display_value'] = $display_value;
-            FWP()->indexer->index_row( $params );
-        }
+		// Pod not found or item does not exist.
+		if ( ! $pod || ! $pod->valid() || ! $pod->exists() ) {
+			return $rows;
+		}
 
-        // google_map
-        elseif ( 'google_map' == $field['type'] ) {
-            if ( isset( $value['lat'] ) && isset( $value['lng'] ) ) {
-                $params['facet_value'] = $value['lat'];
-                $params['facet_display_value'] = $value['lng'];
-                FWP()->indexer->index_row( $params );
-            }
-        }
+		$field_data = $pod->fields( $field_name );
 
-        // text
-        else {
-            $params['facet_value'] = $value;
-            $params['facet_display_value'] = apply_filters( 'facetwp_acf_display_value', $value, $params );
-            FWP()->indexer->index_row( $params );
-        }
-    }
+		// Field not found.
+		if ( ! $field_data ) {
+			return $rows;
+		}
 
+		$field_params = array(
+			'name'    => $field_name,
+			'keyed'   => true,
+			'output'  => 'names', // In the future, add support for slugs=>names.
+		);
 
-    /**
-     * @todo account for Pods fields as the facet "source_other"
-     */
-    public function index_source_other( $value, $params ) {
-        return $value;
-    }
+		$values = $pod->field( $field_params );
+
+		if ( ! is_array( $values ) ) {
+			$values = array( $values );
+		}
+
+		$field_type = $field_data['type'];
+
+		$tableless_field_types = PodsForm::tableless_field_types();
+
+		foreach ( $values as $value => $display_value ) {
+			$row_value = array(
+				'value'         => $value,
+				'display_value' => $display_value,
+			);
+
+			if ( ! in_array( $field_type, $tableless_field_types, true ) ) {
+				// Regular fields don't have a $value, it's just a normal array key (0+).
+				$row_value['value'] = $row_value['display_value'];
+			} elseif ( 0 === $row_value['value'] ) {
+				// Set zero values for tableless fields as no value (null).
+				$row_value['value'] = null;
+			}
+
+			$row = $this->setup_index_row( $row_value, $field, $pod, $params );
+
+			if ( $row ) {
+				$rows[] = $row;
+			}
+		}
+
+		return $rows;
+	}
+
+	/**
+	 * Setup index row for value.
+	 *
+	 * @param array  $row_value Row value data.
+	 * @param string $field     Field name.
+	 * @param Pods   $pod       Pod object.
+	 * @param array  $params    Index parameters.
+	 *
+	 * @return array|false Row data or false if bad value.
+	 *
+	 * @since 0.1
+	 */
+	public function setup_index_row( $row_value, $field, $pod, $params ) {
+		$row = false;
+
+		if ( ! is_scalar( $row_value['value'] ) || ! is_scalar( $row_value['display_value'] ) ) {
+			return $row;
+		}
+
+		$row = $params['defaults'];
+
+		// Set some Pods-related stuff so people can filter our rows and do more with them later.
+		$row['pods_name']    = $pod->pod;
+		$row['pods_type']    = $pod->pod_data['type'];
+		$row['pods_item_id'] = $pod->id();
+
+		// Set values.
+		$row['facet_value']         = $row_value['value'];
+		$row['facet_display_value'] = $row_value['display_value'];
+
+		return $row;
+	}
+
+	/**
+	 * @param array $excluded_fields Excluded fields.
+	 *
+	 * @return array Excluded fields.
+	 *
+	 * @since 0.1
+	 */
+	public function exclude_internal_fields( $excluded_fields ) {
+		/** @var wpdb $wpdb */
+		global $wpdb;
+
+		// Exclude post meta keys from _pods_pod, _pods_field, etc.
+		$sql = "
+			SELECT DISTINCT `pm`.`meta_key`
+			FROM `{$wpdb->postmeta}` AS `pm`
+			LEFT JOIN `{$wpdb->posts}` AS `p`
+				ON `p`.`ID` = `pm`.`post_id`
+			WHERE `p`.`post_type` LIKE '_pods_%'
+		";
+
+		$internal_fields = $wpdb->get_col( $sql );
+
+		$excluded_fields = array_merge( $excluded_fields, $internal_fields );
+
+		// Exclude post meta keys used for relationship order storage.
+		$sql = "
+			SELECT DISTINCT `meta_key`
+			FROM `{$wpdb->postmeta}`
+			WHERE `meta_key` LIKE '_pods_%'
+		";
+
+		$internal_fields = $wpdb->get_col( $sql );
+
+		$excluded_fields = array_merge( $excluded_fields, $internal_fields );
+
+		// Exclude post meta keys for our Pods.
+		$this->setup_fields();
+
+		$internal_fields = array();
+
+		foreach ( $this->fields as $choice_id => $field ) {
+			$internal_fields[] = basename( $choice_id );
+		}
+
+		$excluded_fields = array_merge( $excluded_fields, $internal_fields );
+
+		$excluded_fields = array_unique( array_filter( $excluded_fields ) );
+
+		return $excluded_fields;
+	}
+
+	/**
+	 * Setup Pods object.
+	 *
+	 * @param string $pod_name Pod name.
+	 * @param mixed  $item_id  Item ID.
+	 *
+	 * @return Pods The pod object.
+	 *
+	 * @since 0.1
+	 */
+	protected function setup_pod( $pod_name, $item_id ) {
+		/** @var Pods $pod */
+		static $pod;
+
+		if ( ! $pod || $pod->pod !== $pod_name ) {
+			// Setup Pods object if we need to.
+			$pod = pods( $pod_name, $item_id );
+		} elseif ( (int) $pod->id !== (int) $item_id && $pod->id !== $item_id ) {
+			// Fetch the row if it isn't already the current one
+			$pod->fetch( $item_id );
+		}
+
+		return $pod;
+	}
+
 }
 
 new FacetWP_Pods_Addon();
